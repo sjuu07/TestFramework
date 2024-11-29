2559791277f3655c88c221f308981901682d65ee
Juhi <juhimarysaji15@gmail.com>
2024-11-29 17:56:31 +0530

    message

--- src/test/java/StepDefinition/StepDefs.java
+++ src/test/java/StepDefinition/StepDefs.java
@@ -51,7 +51,6 @@ public class StepDefs {
 			scenario.attach(((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES), "image/png", "Error Screenshot");
 			
 			driver.quit();
-			system.out.println("Browser TearDown");
 			driver=null;
 		}
 	}
