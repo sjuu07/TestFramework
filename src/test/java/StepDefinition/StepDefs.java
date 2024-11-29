b5b44cce0a9c789e73d375eb24016373c6806b98
Juhi <juhimarysaji15@gmail.com>
2024-11-29 17:52:22 +0530

    commit

--- src/test/java/StepDefinition/StepDefs.java
+++ src/test/java/StepDefinition/StepDefs.java
@@ -51,7 +51,7 @@ public class StepDefs {
 			scenario.attach(((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES), "image/png", "Error Screenshot");
 			
 			driver.quit();
-			System.out.println("Browser TearDown");
+			
 			driver=null;
 		}
 	}
